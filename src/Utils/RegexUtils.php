c9ec969f428f1a06f20bdd0716c7b83dac5b7ba8
Gianluca Giovinazzo <ggiovinazzo@mediadata.tv>
2021-09-02 13:37:44 +0200

    Added new generic config to match in RegexUtils + Added required php exts

--- src/Utils/RegexUtils.php
+++ src/Utils/RegexUtils.php
@@ -4,6 +4,7 @@ namespace MediadataTv\Utils;
 
 use function trim;
 use function sprintf;
+use function is_array;
 
 class RegexUtils
 {
@@ -11,36 +12,37 @@ class RegexUtils
      * Converts a generic array of regex config in array of current regexes
      * where key is the pattern and value is the replacement
      *
-     * @param        $config
+     * @param null|array $config
      *
      * @return array
      */
-    public static function configToRegexReplaceArray($config): array
+    public static function configToRegexReplaceArray(?array $config): array
     {
         $out = [];
-        foreach ($config as $c) {
-            $key = $c['from'];
-            if ($key === null || trim($key) === '') {
-                continue;
+        if(is_array($config)) {
+            foreach ($config as $c) {
+                $key = $c['from'];
+                if ($key === null || trim($key) === '') {
+                    continue;
+                }
+                switch ($c['type']) {
+                    case 'contains':
+                        $key = sprintf('/%s/iu', $key);
+                        break;
+                    case 'startsWith':
+                        $key = sprintf('/^%s/iu', $key);
+                        break;
+                    case 'endsWith':
+                        $key = sprintf('/%s$/iu', $key);
+                        break;
+                    case 'exact':
+                    default:
+                        $key = sprintf('/^%s$/iu', $key);
+                        break;
+                }
+                $out[$key] = $c['to'];
             }
-            switch ($c['type']) {
-                case 'contains':
-                    $key = sprintf('/%s/iu', $key);
-                    break;
-                case 'startsWith':
-                    $key = sprintf('/^%s/iu', $key);
-                    break;
-                case 'endsWith':
-                    $key = sprintf('/%s$/iu', $key);
-                    break;
-                case 'exact':
-                default:
-                    $key = sprintf('/^%s$/iu', $key);
-                    break;
-            }
-            $out[$key] = $c['to'];
         }
-
         return $out;
     }
 
@@ -48,51 +50,52 @@ class RegexUtils
      * Converts a generic array of regex config in array of current regexes
      * where key is the pattern and value is the replacement
      *
-     * @deprecated Use MediadataTv\Utils\RegexUtils::configToRegexReplaceArray instead will be removed in v1.1.0
-     *
-     * @param        $config
+     * @param array|null $config
      *
      * @return array
+     * @deprecated Will be removed in v1.1.0 . Use MediadataTv\Utils\RegexUtils::configToRegexReplaceArray instead.
+     *
      */
-    public static function configToRegexArray($config): array
+    public static function configToRegexArray(?array $config): array
     {
-
+        return self::configToRegexReplaceArray($config);
     }
 
     /**
      * Converts a generic array of regex match config in array of current regexes
      *
-     * @param        $config
+     * @param array|null $config
      *
      * @return array
      */
-    public static function configToRegexMatchArray($config): array
+    public static function configToRegexMatchArray(?array $config): array
     {
         $out = [];
-        foreach ($config as $c) {
-            $key = $c['from'];
-            if ($key === null || trim($key) === '') {
-                continue;
-            }
-            switch ($c['type']) {
-                case 'contains':
-                    $key = sprintf('/%s/iu', $key);
-                    break;
-                case 'startsWith':
-                    $key = sprintf('/^%s/iu', $key);
-                    break;
-                case 'endsWith':
-                    $key = sprintf('/%s$/iu', $key);
-                    break;
-                case 'exact':
-                default:
-                    $key = sprintf('/^%s$/iu', $key);
-                    break;
+        if(is_array($config)) {
+            foreach ($config as $c) {
+                $match = $c['from'] ?? null;
+                if ($match === null || trim($match) === '') {
+                    continue;
+                }
+                switch ($c['type']) {
+                    case 'contains':
+                        $out[] = sprintf('/%s/iu', $match);
+                        break;
+                    case 'startsWith':
+                        $out[] = sprintf('/^%s/iu', $match);
+                        break;
+                    case 'endsWith':
+                        $out[] = sprintf('/%s$/iu', $match);
+                        break;
+                    case 'exact':
+                    default:
+                        $out[] = sprintf('/^%s$/iu', $match);
+                        break;
+                }
             }
         }
 
         return $out;
     }
-
 
 }

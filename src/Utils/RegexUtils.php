02bb9ad149f9ff5ac8c9ea25bba26cb061262eab
Gianluca Giovinazzo <ggiovinazzo@mediadata.tv>
2021-09-02 13:22:05 +0200

    Added new generic config to match in RegexUtils + Added required php exts

--- src/Utils/RegexUtils.php
+++ src/Utils/RegexUtils.php
@@ -2,6 +2,7 @@
 
 namespace MediadataTv\Utils;
 
+use function trim;
 use function sprintf;
 
 class RegexUtils
@@ -14,7 +15,7 @@ class RegexUtils
      *
      * @return array
      */
-    public static function configToRegexArray($config)
+    public static function configToRegexReplaceArray($config): array
     {
         $out = [];
         foreach ($config as $c) {
@@ -38,6 +39,56 @@ class RegexUtils
                     break;
             }
             $out[$key] = $c['to'];
+        }
+
+        return $out;
+    }
+
+    /**
+     * Converts a generic array of regex config in array of current regexes
+     * where key is the pattern and value is the replacement
+     *
+     * @deprecated Use MediadataTv\Utils\RegexUtils::configToRegexReplaceArray instead will be removed in v1.1.0
+     *
+     * @param        $config
+     *
+     * @return array
+     */
+    public static function configToRegexArray($config): array
+    {
+
+    }
+
+    /**
+     * Converts a generic array of regex match config in array of current regexes
+     *
+     * @param        $config
+     *
+     * @return array
+     */
+    public static function configToRegexMatchArray($config): array
+    {
+        $out = [];
+        foreach ($config as $c) {
+            $key = $c['from'];
+            if ($key === null || trim($key) === '') {
+                continue;
+            }
+            switch ($c['type']) {
+                case 'contains':
+                    $key = sprintf('/%s/iu', $key);
+                    break;
+                case 'startsWith':
+                    $key = sprintf('/^%s/iu', $key);
+                    break;
+                case 'endsWith':
+                    $key = sprintf('/%s$/iu', $key);
+                    break;
+                case 'exact':
+                default:
+                    $key = sprintf('/^%s$/iu', $key);
+                    break;
+            }
         }
 
         return $out;
